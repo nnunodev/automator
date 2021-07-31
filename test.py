f6c24dada830fbe4796e2d1f526ca685a84d4f06
Nuno Nunes <nunotnunes@outlook.com>
2021-07-31 16:50:25 +0100

    added click on search

--- test.py
+++ test.py
@@ -9,4 +9,9 @@ driver = webdriver.Firefox()
 driver.get("https://youtube.com/")
 driver.find_element_by_xpath(
     '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span').click()
-driver.find_element_by_xpath('//*[@id="search"]')
+searchBox = driver.find_element_by_xpath('//input[@id="search"]')
+searchBox.send_keys('Muse')
+
+searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]').click()
+driver.implicitly_wait(2)
+driver.quit()
