6966f211735e2f2aafd2e35ece8921d6d62e44f2
Nuno Nunes <nunotnunes@outlook.com>
2021-07-31 16:29:16 +0100

    added firefox

--- test.py
+++ test.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from pathlib import Path
 
-chromeDriver = Path('C:\ChromeDriver\chromedriver.exe') 
-driver = webdriver.Chrome(chromeDriver)
-driver.get ("https://youtube.com/")
-driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span').click()
+# chrome driver
+#chromeDriver = Path('C:\ChromeDriver\chromedriver.exe')
+#driver = webdriver.Chrome(chromeDriver)
+driver = webdriver.Firefox()
+driver.get("https://youtube.com/")
+driver.find_element_by_xpath(
+    '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span').click()
 driver.find_element_by_xpath('//*[@id="search"]')
